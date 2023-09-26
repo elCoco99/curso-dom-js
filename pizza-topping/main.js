1eb9acb6f52c93b2e94b355772aa2953da15ad3b
El Coco <145827756+elCoco99@users.noreply.github.com>
2023-09-26 11:06:29 -0500

    agregar ejemplons de manipulacion de clases, atributos de elementos

--- pizza-topping/main.js
+++ pizza-topping/main.js
@@ -1,29 +1,49 @@
-/* Agregar estilos desde CSS */ 
+/* Constantes */ 
 
 const topping = document.querySelector('.topping')
+const topList = document.getElementById('toppings')
+const link = document.getElementsByTagName('a')
 
-topping.style.backgroundColor = 'blue'
-topping.style.textTransform = 'uppercase'
+/* Agregar estilos desde CSS */ 
+
+topping.style.backgroundColor = 'blue'    // modifica el fondo
+topping.style.textTransform = 'uppercase' // modifica la letra a mayus
 
 /* Seleccionar texto */ 
 
-const topList = document.getElementById('toppings')
-
 console.log('> inner Text')
-console.log(topList.innerText)
+console.log(topList.innerText) // muestra el texto de los elem seleccionados
 
 console.log('> text Content')
-console.log(topList.textContent)
+console.log(topList.textContent) // muestra el texto de los elem seleccionados (incluye espacios identados)
 
 console.log('> innerHTML')
-console.log(topList.innerHTML)
+console.log(topList.innerHTML) // muestra html del elemento seleccionado
 
- /* Modificar atributos de elementos HTML */
+/* Modificar atributos de elementos HTML */
 
- const link = document.getElementsByTagName('a')
- console.log(link[0].getAttribute('href'))
+console.log(link[0].getAttribute('href')) // selecciona atributo
 
- link[0].removeAttribute('href')
+link[0].removeAttribute('href') // elimina atributo
+link[0].setAttribute('href', 'elcocodev.com') // crea o modifica si ya existe, el atributo seleccionado
 
- link[0].setAttribute('href', 'elcocodev.com')
+
+/* Agregar y modificar clases */ 
+
+console.log('> classList')
+console.log(topping.classList)
+
+console.log('> Agregar clase')
+topping.classList.add('nueva-clase')
+// se agregó nueva-clase al topping
+console.log('se agregó nueva-clase al topping')
+console.log(document.getElementsByTagName('li'))
+
+
+
+
+
+
+
+
 
