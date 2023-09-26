da6635aff5e180f53c7dd100fdae4f5b0c8c3636
El Coco <145827756+elCoco99@users.noreply.github.com>
2023-09-26 13:40:12 -0500

    ?

--- pizza-topping/main.js
+++ pizza-topping/main.js
@@ -40,11 +40,64 @@ console.log('se agregó nueva-clase al topping')
 console.log(document.getElementsByTagName('li'))
 
  // verificar si existe una clase:
- console.log('> verificar si existe clase nuva-clase')
+ console.log('> verificar si existe clase nueva-clase')
  console.log(topping.classList.contains('nueva-clase'))
 
  // Eliminar una clase
- topping.classList.remove('nueva-clase') 
+ topping.classList.remove('nueva-clase')
+
+/* Crea elementos en el DOM */
+
+const newTopping = document.createElement('li'); // crea nuevo elemento html
+newTopping.classList.add('topping', 'marron') // asigna clases css
+newTopping.innerText = 'Extra Cheese' // asigna texto
+
+
+topList.append(newTopping)// agregar nuevo elemento creado
+
+newTopping.remove() // elimina un elemento
+
+/* Recorrer el DOM */
+
+console.log('> Parent Element') 
+console.log(topList.parentElement) // permite manipular el padre condicionalmente del hijo
+
+console.log('> Parent Node')
+console.log(topList.parentNode) // permite manipular el NODO padre condicionalmente del hijo
+
+console.log('> Parent Element (abuelo)') 
+console.log(topList.parentElement.parentElement)  // accede al abuelo de un elemento
+
+console.log('> Children Element') 
+console.log(topList.children)  // hijos de un elemento
+
+console.log('> Primer Children (NODO)') 
+console.log(topList.firstChild)   // primer hijo de un elemento (nodo)
+
+console.log('> Primer Children (Elemento)') 
+console.log(topList.firstElementChild)  // primer hijo de un elemento (ELEMENTO)
+console.log(topList.children[0]) // primer hijo de un elemento (ELEMENTO)
+
+console.log('> Ultimo Children (NODO)') 
+console.log(topList.lastChild) // ultimo hijo de un elemento (nodo)
+
+console.log('> Ultimo Children (ELEMENTO)') 
+console.log(topList.lastElementChild) // ultimo hijo de un elemento (elemento)
+
+console.log('> Hermano previo de un elemento (NODO)') 
+console.log(topList.previousSibling) // hermano previo (NODO)
+
+console.log('> Hermano previo de un elemento (ELEMENTO)') 
+console.log(topList.previousElementSibling) // hermano previo (elemento)
+
+console.log('> Hermano siguiente de un elemento (nodo)') 
+console.log(topList.nextSibling) // hermano siguiente (nodo)
+
+console.log('> Hermano siguiente de un elemento (elemento)') 
+console.log(topList.nextElementSibling) // hermano siguiente (elemento)
+
+
+
 
 
 
