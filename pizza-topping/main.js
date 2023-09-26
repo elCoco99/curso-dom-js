80021d3a3b5bda2d5d5f4013268001c1f564b24d
El Coco <145827756+elCoco99@users.noreply.github.com>
2023-09-26 09:28:18 -0500

    fondo para contendor en pizza-toppings

---
+++ pizza-topping/main.js
@@ -0,0 +1,5 @@
+const titulo = document.getElementById('titulo')
+console.log('El tipo de dato del ById es ', typeof titulo)
+
+const clase = document.getElementsByClassName('topping')
+console.log(clase[0])
