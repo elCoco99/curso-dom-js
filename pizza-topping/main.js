5bdd1ca3f4fafa23d6f8916fd3dd392809f51717
El Coco <145827756+elCoco99@users.noreply.github.com>
2023-09-26 10:48:44 -0500

    agregar ejemplos de manipulacion del dom con JS

--- pizza-topping/main.js
+++ pizza-topping/main.js
@@ -1,4 +1,29 @@
+/* Agregar estilos desde CSS */ 
+
 const topping = document.querySelector('.topping')
 
 topping.style.backgroundColor = 'blue'
 topping.style.textTransform = 'uppercase'
+
+/* Seleccionar texto */ 
+
+const topList = document.getElementById('toppings')
+
+console.log('> inner Text')
+console.log(topList.innerText)
+
+console.log('> text Content')
+console.log(topList.textContent)
+
+console.log('> innerHTML')
+console.log(topList.innerHTML)
+
+ /* Modificar atributos de elementos HTML */
+
+ const link = document.getElementsByTagName('a')
+ console.log(link[0].getAttribute('href'))
+
+ link[0].removeAttribute('href')
+
+ link[0].setAttribute('href', 'elcocodev.com')
+
