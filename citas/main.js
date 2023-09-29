c4e05e7469c54f9a535e46b9b8cdec39216beb97
El Coco <145827756+elCoco99@users.noreply.github.com>
2023-09-28 19:11:27 -0500

    agregar primera parte del proyecto citas
    se instalo bootstrap y ahora hay un chorreron de elementos nuevos por el node_modules // pendiente de eliminar

---
+++ citas/main.js
@@ -0,0 +1,19 @@
+/* Contextos */
+
+//boton
+let botonElem = document.querySelector('button')
+
+//texto de la cita
+let cita = document.getElementById('cita')
+
+//texto autor
+let aut = document.getElementById('autor')
+
+//generar número aleatorio
+
+function randomNumber (min, max) {
+
+  return Math.floor(Math.random()* (max - min) + min)
+}
+
+console.log(rando)
